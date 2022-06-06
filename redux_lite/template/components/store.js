e994377ad4479ffdf82b7e90800a190ac9fc5b47
apodavonov3616 <apodavonov3616@DESKTOP-DU4IAMR.localdomain>
2022-06-06 13:51:34 -0400

    started redux lite

---
+++ redux_lite/template/components/store.js
@@ -0,0 +1,17 @@
+
+export default class Store {
+    constructor(rootReducer) {
+        this.rootReducer = rootReducer;
+        this.state = {};
+    }
+
+    getState() {
+        return Object.assign({}, this.state);
+    }
+
+    combineReducers() {
+
+    }
+
+    
+}
